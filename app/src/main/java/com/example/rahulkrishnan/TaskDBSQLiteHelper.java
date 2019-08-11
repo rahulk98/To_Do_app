9ff6814afd9502290ed26225415a19a993531ac7
rahulk98 <rahul.krishnan27@gmail.com>
2019-08-11 14:06:21 +0530

    added date queries

--- app/src/main/java/com/example/rahulkrishnan/TaskDBSQLiteHelper.java
+++ app/src/main/java/com/example/rahulkrishnan/TaskDBSQLiteHelper.java
@@ -1,12 +1,13 @@
 package com.example.rahulkrishnan;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class TaskDBSQLiteHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "tasks_database";
+    public static final String DATABASE_NAME = "tasks_database.db";
 
     public TaskDBSQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -24,4 +25,21 @@ public class TaskDBSQLiteHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
+    public Cursor getTodayTasks() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + DBTask.Tasks.TABLE_NAME + " WHERE date(strftime('%m/%Y/%d', " + DBTask.Tasks.COLUMN_TASK_DATE + ", 'localtime')) = date('now', 'localtime') order by id desc", null);
+        return res;
+    }
+
+    public Cursor getTomorrowTasks() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + DBTask.Tasks.TABLE_NAME + " WHERE date(strftime('%m/%Y/%d', " + DBTask.Tasks.COLUMN_TASK_DATE + ", 'localtime')) = date('now', '+1 day','localtime') order by id desc", null);
+        return res;
+    }
+
+    public Cursor getUpcomingTasks() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + DBTask.Tasks.TABLE_NAME + " WHERE date(strftime('%m/%Y/%d', " + DBTask.Tasks.COLUMN_TASK_DATE + ", 'localtime')) > date('now', '+1 day','localtime') order by id desc", null);
+        return res;
+    }
 }
